4544266a00509e257f292d0b9d142d723d3a6218
LukasG62 <98223802+LukasG62@users.noreply.github.com>
2022-02-06 12:32:28 +0100

    Add file via upload

---
+++ diag/diag.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "avalam.h"
+#include "topologie.h"
+
+#define DEFAULT_NAME "diag.js"
+#define DEFAULT_NOTE ""
+#define MAX_FEN 100
+
+#define U 1
+#define D 2
+#define T 3
+#define Q 4
+#define C 5
+#define BONUS 0
+#define MALUS 1
+
+void placer_colonne(T_Position *p, octet col, octet nb, octet c);
+void placer_evolution(T_Position *p, octet col, octet type, octet c);
+int main(int argc, char *argv[]){
+    T_Position plateau;
+    
+    char fen[MAX_FEN];
+    int diag_id; // numéro du diagramme
+    octet j= 0; // compteur pour parcourir le fen
+    octet i = 0; // compteur pour parcourir le plateau
+    octet nb_evolution; // pour compter le nombre de bonus 
+    
+    // vérification du nombre d'arguments
+    if(argc != 3) {
+        return printf("Argument invalide ! \n %s <Numero diagramme> <code fen> \n", argv[0]);
+        return 1;
+    }
+    // récupération des arguments
+    else {
+        diag_id = atoi(argv[1]);
+        strcpy(fen, argv[2]);
+    }
+    // parcours du fen
+    while(fen[j]) {
+        // TODO reflechir à comment faire les vérifications
+        switch(fen[j]){
+            // cas pour les pions jaunes
+            case 'u':
+                placer_colonne(&plateau, i, U, JAU);
+                i++; 
+                break;
+            
+            case 'd':
+                placer_colonne(&plateau, i, D, JAU);
+                i++;
+                break;
+                
+            case 't':
+                placer_colonne(&plateau, i, T, JAU);
+                i++;
+                break;
+            
+            case 'q':
+                placer_colonne(&plateau, i, Q, JAU);
+                i++;
+                break;
+            
+            case 'c':
+                placer_colonne(&plateau, i, C, JAU);
+                i++;
+                break;
+                
+            case 'b':
+                placer_evolution(&plateau, i-1, BONUS, JAU);
+                nb_evolution++;
+                break;
+                
+            case 'm':
+                placer_evolution(&plateau, i-1, MALUS, JAU);
+                nb_evolution++;
+                break;
+            
+            // cas pour les pion Rouge
+            case 'U':
+                placer_colonne(&plateau, i, U, ROU);
+                i++;
+                break;
+            
+            case 'D':
+                placer_colonne(&plateau, i, D, ROU);
+                i++;
+                break;
+            
+            case 'T':
+                placer_colonne(&plateau, i, T, ROU);
+                i++;
+                break;    
+            
+            case 'Q':
+                placer_colonne(&plateau, i, Q, ROU);
+                i++;
+                break; 
+            
+            case 'C':
+                placer_colonne(&plateau, i, C, ROU);
+                i++;
+                break;
+            
+            case 'B':
+                 placer_evolution(&plateau, i-1, BONUS, ROU);
+                 nb_evolution++;
+                 break;
+            
+            case 'M':
+                 placer_evolution(&plateau, i-1, MALUS, ROU);
+                 nb_evolution++;
+                 break;
+            
+            //TODO cas pour le trait (caractère après l'espace)
+            case ' ':
+                break;
+            
+            //TODO Caractère inconnu ou gestion du nombre de case vide ??
+            default:
+                break;
+            
+        }
+        j++;
+    }
+    for(i; i < NBCASES; i++) placer_colonne(&plateau, i, VIDE, VIDE);
+    afficherPosition(plateau); // à retirer
+}
+
+void placer_colonne(T_Position *p,octet col, octet nb, octet c){
+    // tu lui donnes le plateau, la colonne, sa valeur, et la couleur et elle te place le pion
+    // passage par adresse donc p est modifié
+    p->cols[col].nb = nb;
+    p->cols[col].couleur = c;
+    printf3("Placement [%d | [%s : %d]\n", col, COLNAME(c), nb); // printf pour la version debug
+}
+
+void placer_evolution(T_Position *p, octet col, octet type, octet c) {
+    // tu lui donnes le plateau, la colonne, et le type (BONUS ou MALUS) et elle te place le pion évolution
+    // passage par adresse donc p est modifié
+    if(type == MALUS){
+        //On place notre pion evolution dans le plateau selon sa couleur
+        if(c == JAU) p->evolution.malusJ = col;
+        else if(c == ROU) p->evolution.malusR = col;
+    }
+    // Sinon c'est un bonus
+    else {
+        //On place notre pion evolution dans le plateau selon sa couleur
+        if(c == JAU) p->evolution.bonusJ = col;
+        else if(C == ROU) p->evolution.bonusR = col;
+    }
+}
---
+++ standalone/standalone.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <string.h>
+#include "avalam.h"
+#include "topologie.h"
+
+#define DEFAULT_LOCATION "web/data/refresh-data.js"
+#define MAX_FILELOCATION 100 // Taille max du chemin spécifié par ligne de commande
+
+
+int ecraserJson(T_Position p, T_Score s, char *flocation);
+
+int main(int argc, char *argv[]) {
+    // Declaration et initialisation des variables du jeu
+    T_Position plateau = getPositionInitiale();
+    T_Score score;
+    T_ListeCoups coupsLegaux = getCoupsLegaux(plateau);
+    T_Coup coup;
+    octet pbonus; // position des bonus saisie par l'utilisateur
+
+    // Variable contenant le chemin du fichier json
+    char file_location[MAX_FILELOCATION] = DEFAULT_LOCATION;
+    
+    // Si il n'y pas au moins un argument alors on l'indique et on prend le chemin par défaut
+    if(argc < 2) printf("[Warning] Chemin non spécifié \n\n");
+    else{
+	// Si il y a au moins un argument alors on vérifie sa taille
+	if(strlen(argv[1]) > MAX_FILELOCATION){
+	    // Dans le cas d'un paramètre non valide on termine le programme
+	    printf("Chemin spécifié trop long ! Fin du programme");
+	    return 1;
+	}
+	strcpy(file_location, argv[1]);
+    }
+	
+    printf("\t\t EVA-LAM PROTOtYPE 01\n");
+    printf("\t\t --------------------\n");
+    printf("Emplacement du fichier js : '%s' \n", file_location);
+    printf("DEBUG : OFF"); // TODO debug mode
+    printf("\n");
+    printf("Placement des bonus/malus :\n");
+    
+    // Placement des bonus/malus avec vérification des saisies
+    do{
+	printf("\tbonusJ :");
+	scanf("%d",&pbonus);
+	plateau.evolution.bonusJ = pbonus;
+	printf("la valeur donnée est : %d\n",plateau.evolution.bonusJ);
+    }while(plateau.cols[pbonus].couleur != JAU);
+    
+    do{
+	printf("\tbonusR :");
+	scanf("%d",&pbonus);
+	plateau.evolution.bonusR = pbonus;
+    }while(plateau.cols[pbonus].couleur != ROU );
+    
+    printf("la valeur donnée est : %d\n",plateau.evolution.bonusR);
+
+    do{
+	printf("\tmalusR :");
+	scanf("%d",&pbonus);
+	plateau.evolution.malusR = pbonus;
+    }while((plateau.evolution.malusR == plateau.evolution.bonusR) || (plateau.cols[pbonus].couleur != ROU));
+    printf("la valeur donnée est : %d\n",plateau.evolution.malusR);
+    
+    do{
+    	printf("\tmalusJ :");
+    	scanf("%d",&pbonus);
+    	plateau.evolution.malusJ = pbonus;
+    }while((plateau.evolution.malusJ == plateau.evolution.bonusJ ) || (plateau.cols[pbonus].couleur != JAU));
+    printf("la valeur donnée est : %d\n",plateau.evolution.malusJ);
+    
+    score = evaluerScore(plateau); // Maintenant que les pions évolution sont placé on peut initialisé le score
+	
+    //afficherPosition(plateau); // A retirer
+    ecraserJson(plateau, score, file_location); // Mise à jour du fichier json
+    
+    while(coupsLegaux.nb){
+	// On affiche le score
+        afficherScore(score);
+	//afficherPosition(plateau);
+	printf("Trait aux %ss \n",COLNAME(plateau.trait));
+	
+	printf("\tQuel est la position du pion à déplacer :");
+	scanf("%d",&coup.origine);
+	
+        printf("\tQuel est la position du pion d'arrivée :");   
+        scanf("%d",&coup.destination);
+        
+	printf("\tLe coup à jouer est : %d -> %d\n",coup.origine,coup.destination);
+        plateau = jouerCoup(plateau,coup.origine,coup.destination);
+		
+	coupsLegaux = getCoupsLegaux(plateau);
+        printf("\tIl y a %d coups possibles\n", coupsLegaux.nb);
+        printf("\n\n\n\n");
+	
+	score=evaluerScore(plateau);
+	ecraserJson(plateau, score, file_location);
+		
+    }
+	
+	return 0;
+}
+
+int ecraserJson(T_Position p, T_Score s, char *flocation){
+	octet i;
+	FILE *fic=NULL; // Pointeur de notre fichier
+	fic=fopen(flocation,"w"); // Ouverture du fichier en mode écriture avec écrasement
+	
+	if (fic==NULL) return 0;
+	
+	else {
+		fprintf(fic,"traiterJson({\n %s:%d,\n %s:%d,\n %s:%d,\n %s:%d,\n %s:%d,\n %s:%d,\n %s:%d,\n %s:%d,\n %s:%d,\n %s:[",
+																    STR_TURN,p.trait,
+																    STR_SCORE_J,s.nbJ,
+																    STR_SCORE_J5,s.nbJ5,
+																    STR_SCORE_R,s.nbR,
+																    STR_SCORE_R5,s.nbR5,
+																    STR_BONUS_J,p.evolution.bonusJ,
+																    STR_BONUS_R,p.evolution.bonusR,
+																    STR_MALUS_R,p.evolution.malusR,
+																    STR_MALUS_J,p.evolution.malusJ,
+																    STR_COLS
+		);
+
+		fprintf(fic,"{%s:%d, %s:%d,}",STR_NB,p.cols[0].nb,STR_COULEUR,p.cols[0].couleur);
+		for (i=1;i<NBCASES;i++) fprintf(fic,",\n\t{%s:%d, %s:%d,}",STR_NB,p.cols[i].nb,STR_COULEUR,p.cols[i].couleur);
+		
+		fprintf(fic,"]});");
+		fclose(fic);
+		return 1;
+	}
+	
+}
