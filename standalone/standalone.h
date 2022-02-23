79f637db300a4421a477ba017dd843983c4a2694
LukasG62 <lukas.grando.62@gmail.com>
2022-02-23 10:57:26 -0500

    ajout des cartouches des fonctions

--- standalone/standalone.h
+++ standalone/standalone.h
@@ -9,9 +9,40 @@
 
 #define ISDEBUG() (DEBUGVALUE ? "ON" : "OFF")
 
+//##################################################################################################
+//Fonction : ecraserJson                                                                           #
+// Description : Permet de mettre à jour le fichier json                                           #                   
+// Entrees : p:Le plateau de jeu, s: le score, flocation: le chemin du fichier                     #
+// Sorties : (/)                                                                                   #
+// Retourne : 0:échec, 1:succés                                                                    #
+//##################################################################################################
 int ecraserJson(T_Position p, T_Score s, char *flocation);
+
+
+//##################################################################################################
+//Fonction : saisirEvolution                                                                       #
+// Description : Permet de saisir les 4 pions                                                      #                   
+// Entrees : p:Le plateau de jeu, s: le score, flocation: le chemin du fichier                     #
+// Sorties : (/)                                                                                   #
+// Retourne : 0:échec, 1:succés                                                                    #
+//##################################################################################################
 void saisirEvolution(T_Position *p);
 
+
+//##################################################################################################
+//Fonction : initScore                                                                             #
+// Description : Fonction pour initialiser la struture T_Score                                     #                   
+// Entrees : *ptrE:Pointeur d'un T_Score                                                           #
+// Sorties : la struture pointée est modifiée                                                      #
+// Retourne : (/)                                                                                  #
+//##################################################################################################
 void initScore(T_Score *ptrS);
 
+//##################################################################################################
+//Fonction : initEvolution                                                                         #
+// Description : Fonction pour initialiser la struture T_evolution                                 #                   
+// Entrees : *ptrE:Pointeur d'un T_Evolution                                                       #
+// Sorties : la struture pointée est modifiée                                                      #
+// Retourne : (/)                                                                                  #
+//##################################################################################################
 void initEvolution(T_Evolution *ptrE);
