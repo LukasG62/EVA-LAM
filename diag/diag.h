ba1c4e1b5ef5900cf1b9b7258eaa13d2b200d23e
LukasG62 <lukas.grando.62@gmail.com>
2022-02-23 10:29:08 -0500

    Ajout des cartouches des fonctions

--- diag/diag.h
+++ diag/diag.h
@@ -19,9 +19,41 @@
 #define MALUS 1
 #define EVOLNAME(type) (type ? "malus" : "bonus")
 
-
+//##################################################################################################
+//Fonction : placer_colonne                                                                        #
+// Description : Procédure qui permet de placer une colonne                                        #                   
+// Entrees : *p:Pointeur d'un T_Position, col: numéro de la colonne, nb: Nombre de pions           #
+//		     c: Couleur du pion                                                                    #
+// Sorties : la struture p est modifié                                                             #
+// Retourne : (/)                                                                                  #
+//##################################################################################################
 void placer_colonne(T_Position *p, octet col, octet nb, octet c);
+
+//##################################################################################################
+//Fonction : placer_evolution                                                                      #
+// Description : Procédure qui permet de placer un pions évolution dans une colonne                #                   
+// Entrees : *p:Pointeur d'un T_Position, col: numéro de la colonne, type: 0=BONUS 1=MALUS         #
+//		     c: Couleur du pion                                                                    #
+// Sorties : la struture p est modifié                                                             #
+// Retourne : (/)                                                                                  #
+//##################################################################################################
 void placer_evolution(T_Position *p, octet col, octet type, octet c);
+
+//##################################################################################################
+//Fonction : generer_json                                                                          #
+// Description : Fonction qui permet de génerer un fichier javascript utilisable par diag.html     #                   
+// Entrees : *p:Pointeur d'un T_Position, diag_id: numéro du diagramme, fen: le code fen           #
+//		     comm: commentaire du diagramme, flocation: Chemin du fichier                          #
+// Sorties : (/)                                                                                   #
+// Retourne : 0:échec de la génération 1:succès                                                    #
+//##################################################################################################
 int generer_json(T_Position p, int diag_id,char *fen,char *comm, char *flocation);
 
+//##################################################################################################
+//Fonction : placer_evolution                                                                      #
+// Description : Fonction pour initialiser la struture T_evolution                                 #                   
+// Entrees : *ptrE:Pointeur d'un T_Evolution                                                       #
+// Sorties : la struture pointé                                                                    #
+// Retourne : (/)                                                                                  #
+//##################################################################################################
 void initEvolution(T_Evolution *ptrE);
